9347a6a40d8f9534b66fa38dce11a7796abab20c
yashpro <eryash@proteamedge.com>
2021-02-17 15:40:25 +0530

    update
    update

---
+++ components/LoginView.js
@@ -0,0 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState,Component  } from 'react';
+
+import { StyleSheet, Text, TextInput, View, Image, Button, Alert} from 'react-native';
+import {globals} from '../styles/globals';
+
+
+class LoginView extends Component {
+	
+	state = { emailEmpty: false,passwordEmpty:false,invalidEmail:false,invalidPassword:false };
+	isValidPassword(str) {
+		
+			if(str.length>=8)
+				return true;
+			else
+				return false;
+		
+	}
+	isEmailAddress(str) {
+		
+		var pattern =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		return pattern.test(str);  // returns a boolean 
+		
+	}
+	formsubmission = (e) => {
+		let error = 0;
+		let email=this.state.email;
+		let password_field = this.state.password_field;
+
+		if(typeof email === "undefined") {
+			error = 1;
+			this.setState({ emailEmpty: true });
+		}
+		else {
+			if(this.isEmailAddress(email)==false) {
+				error = 1;
+				this.setState({ invalidEmail: true });
+			}
+			else {
+				this.setState({ invalidEmail: false });
+			}
+			this.setState({ emailEmpty: false });
+		}
+		if(typeof password_field === "undefined") {
+			error = 1;
+			this.setState({ passwordEmpty: true });
+		}
+		else {
+			if(this.isValidPassword(password_field)==false) {
+				error = 1;
+				this.setState({ invalidPassword: true });
+			}
+			else {
+				this.setState({ invalidPassword: false });
+			}
+			this.setState({ passwordEmpty: false });
+		} 
+		if(error==0)
+		alert("All good form can be submitted!!");
+    }
+  render() {
+        return (
+     <View style={globals.container}>
+	
+            <Image style={globals.logo} source={require('../assets/logo/logo.png')} />
+			
+			<TextInput  onChangeText={(email) => this.setState({email})}   style={globals.inputs}
+         placeholder="Email"
+       />
+	    <Text style={globals.errors}>
+		{this.state.emailEmpty ? " Email is a required field." : null}
+		{this.state.invalidEmail ? " Email is invalid." : null}
+		</Text>
+     <TextInput secureTextEntry={true} onChangeText={(password_field) => this.setState({password_field})}  style={globals.inputs} 
+         placeholder="Password"
+       />  
+	     <Text style={globals.errors}>
+						{this.state.passwordEmpty ? " Password is a required field." : null}
+						{this.state.invalidPassword ? " Password must be 8 characters long." : null}
+					</Text>
+  <Button
+		 onPress={this.formsubmission}
+         style={globals.loginButton}
+		type="submit"
+         title="Login"
+       />
+
+  </View>
+  );
+  }
+}
+export default LoginView;
---
+++ styles/globals.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native'
+
+const globals = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+   },
+  logo: {
+      width: 270,
+      height: 60,
+      resizeMode: 'contain',
+      marginBottom: 25,
+  },
+  inputs:{
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 5,
+	 alignSelf: 'stretch',
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 17,
+    padding: 10
+  },
+   Outer:{
+
+    padding: '0 1rem'
+  },
+
+  loginButton:{
+   
+  },
+  errors:{
+	  color:"red",
+	  marginBottom:10
+  },
+})
+
+export {globals}
